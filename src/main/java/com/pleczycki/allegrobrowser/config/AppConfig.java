7303d2a47ab0546c7d85d512d1ed66af235843e2
Patryk <patryk.leczycki1@gmail.com>
2021-04-25 23:09:26 +0200

    added base classes and config files

---
+++ src/main/java/com/pleczycki/allegrobrowser/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.pleczycki.allegrobrowser.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return restTemplateBuilder.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
